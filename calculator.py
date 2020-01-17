ac012dd75011e06c87820f5f7d0029e9343acb86
Tony Dagher <tydagher@gmail.com>
2020-01-17 12:57:08 -0500

    Finished calculator.py

--- calculator.py
+++ calculator.py
@@ -20,35 +20,43 @@ def divide(a, b):
 
 
 a, b = 47, 7
-print("{} + {} = {}".format(a, b, add(a, b)[0]))
+print("{} {} {} = {}".format(a, add(a,b)[1], b, add(a, b)[0]))
 """
 
 
-def add(x, y):
-    z = x + y
-    print("{} + {} = {}".format(x, y, z))
-    return z
+def add(a, b):
+    c = a + b
+    return c, "+"
 
 
-def subtract(x, y):
-    z = x - y
-    print("{} - {} = {}".format(x, y, z))
-    return z
+def subtract(a, b):
+    c = a - b
+    return c, "-"
 
 
-def multiply(x, y):
-    z = x*y
-    print("{} x {} = {}".format(x, y, z))
-    return z
+def multiply(a, b):
+    c = a * b
+    return c, "*"
 
 
-inp = input("enter a letter: ")
+def divide(a, b):
+    c = a / b
+    return c, "/"
+
+
+inp = input("Input letter to define operation: ")
+num1 = int(input("enter number 1: "))
+num2 = int(input("enter number 2: "))
 print("You entered {}".format(inp))
 if inp == "a":
-    d = add(56, 73)
+    d = add(num1, num2)
 elif inp == "s":
-    d = subtract(56, 73)
+    d = subtract(num1, num2)
 elif inp == "m":
-    d = multiply(56, 73)
+    d = multiply(num1, num2)
+elif inp == "d":
+    d = divide(num1, num2)
 else:
     print("The {} command is not recognized.".format(inp))
+
+print("{} {} {} = {}".format(num1, d[1], num2, d[0]))
